67790abdd9f62519b988ddced8243e079ba39ec2
Anton Shchukin <a.a.shchukin@gmail.com>
2024-09-30 22:21:34 +0700

    build

--- build/scripts/scripts.js
+++ build/scripts/scripts.js
@@ -229,5 +229,228 @@
         }
     });
 
+
+
+    /* Инпуты */
+
+    /* Select placeholder */
+    function selectPlaceholder($element) {
+        if ($element.val() === 'placeholder') {
+            $element.parent('.input').addClass('input--placeholder-is-chosen');
+        } else {
+            $element.parent('.input').removeClass('input--placeholder-is-chosen');
+        }
+    }
+
+    $('select.input__widget').each(function () {
+        selectPlaceholder($(this));
+    }).on('change', function () {
+        selectPlaceholder($(this));
+    });
+
+    /* Expanding textarea */
+    function expandTextarea($element) {
+        $element.css('height', 'auto');
+        $element.css('height', ($element[0].scrollHeight + 2 * parseInt($element.css('border-width'), 10)) + 'px');
+    }
+
+    const $expandableInputs = $('.input--expandable .input__widget');
+
+    $expandableInputs.each(function () {
+        expandTextarea($(this));
+    }).on('input', function () {
+        expandTextarea($(this));
+    });
+
+    $(window).on('resize', function () {
+        $expandableInputs.each(function () {
+            expandTextarea($(this));
+        });
+    });
+    /* Error field */
+    $('.input__widget').on('focus', function () {
+        $(this).parents('.input').removeClass('input--error');
+        $(this).parents('.input').nextUntil(':not(.helper--error)').remove();
+    });
+
+
+
+
+    /* Форма */
+
+    /* Уведомления */
+    document.querySelectorAll('.alert').forEach(function (alert) {
+        alert.addEventListener('click', function () {
+            alert.style.display = 'none';
+        });
+    });
+
+    document.addEventListener('keyup', function (event) {
+        if (event.keyCode === 27) {
+            document.querySelectorAll('.alert').forEach(function (alert) {
+                alert.style.display = 'none';
+            });
+        }
+    });
+
+
+    /* Сама форма */
+    document.querySelectorAll('[data-js-form]').forEach(function (form) {
+        const subscriptionInputs = form.querySelectorAll('.input, .choice');
+        const subscriptionSubmit = form.querySelector('[data-js-submit]');
+        const subscriptionSuccessAlert = document.querySelector('[data-js-alert-success]');
+        const subscriptionFailureAlert = document.querySelector('[data-js-alert-failure]');
+
+        /* Состояния инпутов (на время отправки формы инпуты должны блокироваться) */
+        function disableSubscriptionInputs() {
+            subscriptionInputs.forEach((input) => {
+                input.querySelector('.input__widget, .choice__widget').setAttribute('disabled', 'disabled');
+            });
+        }
+
+        function enableSubscriptionInputs() {
+            subscriptionInputs.forEach((input) => {
+                input.querySelector('.input__widget, .choice__widget').removeAttribute('disabled');
+            });
+        }
+
+        /* Очистка инпутов (после успешной отправки форму должны очищаться) */
+        function cleanSubscriptionInputs() {
+
+            subscriptionInputs.forEach((input) => {
+
+                /* Есть три варианта инпутов: */
+
+                // 1) Текстовые инпуты внутри компонента .input (это всё, что не тег <select>):
+                const textInputs = input.querySelector('.input__widget:not(select)');
+                if (textInputs) {
+                    textInputs.value = '';
+                }
+
+                // 2) <select> внутри .input
+                const selects = input.querySelector('select.input__widget');
+                if (selects) {
+                    const placeholderOption = Array.from(selects.options).find(option => option.getAttribute('value') === 'placeholder');
+                    if (placeholderOption) {
+                        selects.value = placeholderOption.value;
+                        input.classList.add('input--placeholder-is-chosen');
+                    }
+                }
+
+                // 3) .choice__widget
+                const choices = input.querySelector('.choice__widget');
+                if (choices) {
+                    choices.checked = false;
+                }
+            });
+        }
+
+        /* Состояния кнопки */
+        function changeSubmitStateToLoading() {
+            subscriptionSubmit.classList.add('button--loading');
+            subscriptionSubmit.setAttribute('disabled', 'disabled');
+        }
+
+        function changeSubmitStateToSuccess() {
+            subscriptionSubmit.classList.remove('button--loading');
+            subscriptionSubmit.classList.add('button--success');
+            subscriptionSubmit.setAttribute('disabled', 'disabled');
+        }
+
+        function changeSubmitStateToFailure() {
+            subscriptionSubmit.classList.remove('button--loading');
+            subscriptionSubmit.classList.add('button--warning');
+            subscriptionSubmit.setAttribute('disabled', 'disabled');
+        }
+
+        function changeSubmitStateToPristine() {
+            subscriptionSubmit.classList.remove('button--loading', 'button--success', 'button--warning');
+            subscriptionSubmit.removeAttribute('disabled');
+        }
+
+        /* Если пользователь начал взаимодействовать с инпутами, то убираем уведомления с прошлой попытки отправки: */
+        subscriptionInputs.forEach(input => input.addEventListener('input', () => {
+            subscriptionFailureAlert.style.display = 'none';
+        }));
+
+
+        /* Отправка */
+        form.addEventListener('submit', function (event) {
+            event.preventDefault();
+
+            /* Если с прошлой попытки висит уведомление об ошибке: */
+            subscriptionFailureAlert.style.display = 'none';
+
+            /* Начинаем отправку данных, для начала блокируем форму */
+            disableSubscriptionInputs();
+            changeSubmitStateToLoading();
+
+            /* Представим, что 3000ms отправляем данные */
+            setTimeout(function () {
+
+                /* ... дальше развилка, пусть для примера будет рандом 50/50: */
+
+                // Если данные успешно отправлены
+                if (Math.random() < 0.5) {
+
+                    // показываем зелёное уведомление:
+                    subscriptionSuccessAlert.style.display = 'block';
+
+                    // показываем галочку на кнопке:
+                    changeSubmitStateToSuccess();
+
+                    // и то и другое на 4.5 секунды:
+                    setTimeout(function () {
+                        subscriptionSuccessAlert.style.display = 'none';
+                        changeSubmitStateToPristine();
+                        enableSubscriptionInputs();
+
+                        //и очищаем форму:
+                        cleanSubscriptionInputs();
+                    }, 4500);
+
+                }
+
+                // Если произошла ошибка
+                else {
+
+                    // показываем красное уведомление
+                    subscriptionFailureAlert.style.display = 'block';
+
+                    // Показываем восклицательный знак на кнопке:
+                    changeSubmitStateToFailure();
+
+                    // В данном случае всего 2 секунды, чтобы пользователь мог быстро вернуться к работе с формой.
+                    // Уведомление в этом случае НЕ убираем -- пусть висит, пока пользователь не увидит и явно не закроет, или не начнёт заново заполнять форму / попытается отправить:
+                    setTimeout(function () {
+                        changeSubmitStateToPristine();
+                        enableSubscriptionInputs();
+                    }, 2000);
+
+                }
+
+            }, 3000);
+
+        });
+    });
+
+
+    /* Переключатель в групповой заявке */
+
+    document.addEventListener("DOMContentLoaded", function () {
+
+        const switchItems = document.querySelectorAll('[data-js-application-performances], [data-js-application-excursions]');
+        const selectField = document.querySelector('[data-js-application-select-show]');
+
+        switchItems.forEach((item) => {
+            item.addEventListener('click', function () {
+                switchItems.forEach(btn => btn.classList.remove('switch__item--current'));
+                this.classList.add('switch__item--current');
+                selectField.style.display = this.hasAttribute('data-js-application-performances') ? 'block' : 'none';
+            });
+        });
+    });
+
+
 })(jQuery);
 
