7c11725564a6b31750a3bd904047480ec71e5ea4
Anton Shchukin <a.a.shchukin@gmail.com>
2024-10-14 18:29:52 +0700

    video in slide draft

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -141,16 +141,19 @@
 
         /* Swiper для Intro */
 
+        let videoDuration;
+        const introDuration = 4000;
+
         document.querySelectorAll('.carousel--js-init-intro').forEach(($carousel) => {
 
             const progressCircle = $carousel.querySelector(".carousel__progress");
 
-            new Swiper($carousel.querySelector('.swiper'), {
+            const swiper = new Swiper($carousel.querySelector('.swiper'), {
                 slidesPerView: 1,
                 slidesPerGroup: 1,
 
                 autoplay: {
-                    delay: 10000,
+                    delay: introDuration,
                     disableOnInteraction: true
                 },
 
@@ -173,6 +176,22 @@
                         pagination: false
                     },
                 },
+            });
+
+
+            swiper.on('slideChange', function () {
+                const currentSlide = swiper.slides[swiper.activeIndex];
+                const video = currentSlide.querySelector('video');
+
+                if (video) {
+                    swiper.autoplay.stop();
+                    video.play();
+                    videoDuration = video.duration * 1000;
+                    swiper.params.autoplay.delay = videoDuration;
+                    swiper.autoplay.start();
+                } else {
+                    swiper.params.autoplay.delay = introDuration;
+                }
             });
         });
 
