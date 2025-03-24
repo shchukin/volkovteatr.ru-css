3ee3897100790ef7f46791a0ddd6af3b5edc5571
Anton Shchukin <a.a.shchukin@gmail.com>
2025-03-24 17:35:48 +0700

    build

--- build/scripts/scripts.js
+++ build/scripts/scripts.js
@@ -219,8 +219,6 @@
 
 
 
-
-
         /* Swiper для Stories */
 
         document.querySelectorAll('.carousel--js-init-stories').forEach(($carousel) => {
@@ -276,6 +274,43 @@
                         allowTouchMove: false, // Disable all touch and mouse drag interactions
                     }
                 },
+            });
+        });
+
+
+
+        /* Swiper для участия */
+
+        document.querySelectorAll('.carousel--js-init-participation').forEach(($carousel) => {
+
+            new Swiper($carousel.querySelector('.swiper'), {
+                slidesPerView: "auto",
+                slidesPerGroup: 3,
+                spaceBetween: 20,
+
+                pagination: {
+                    el: $carousel.querySelector('.carousel__pagination'),
+                    type: "fraction",
+                },
+
+                navigation: {
+                    prevEl: $carousel.querySelector('.carousel__button--prev'),
+                    nextEl: $carousel.querySelector('.carousel__button--next'),
+                },
+
+                // breakpoints: {
+                //     740: {
+                //         slidesPerView: 2,
+                //         slidesPerGroup: 2,
+                //         pagination: {
+                //             el: $carousel.querySelector('.carousel__pagination'),
+                //             type: "bullets",
+                //             bulletClass: 'carousel__bullet',
+                //             bulletActiveClass: 'carousel__bullet--current',
+                //             clickable: true
+                //         },
+                //     },
+                // },
             });
         });
     }
@@ -373,22 +408,35 @@
         callbacks: {
             open: function () {
 
+                const handler = this.st.el[0];
+                const instance = this.wrap[0];
+
                 // Перезапускаем обсчёт expanding textareas для инстансов внутри открытой модалки
-                const instance = $.magnificPopup.instance;
-                const modalContent = instance.content[0];
-                const textareas = $(modalContent).find('.input--expandable .input__widget');
+                const $textareas = $(instance).find('.input--expandable .input__widget');
 
-                textareas.each(function () {
-                    expandTextarea($(this));
-                });
-
-                /* Шапка фиксированная, ей тоже надо корректировать пропавшее пространство под скроллбаром */
-                $fixedHeader.css({'margin-right': scrollWidth});
-
-                /* Если проставлена опция mfp-handler-left-side (класс на хендлере), то её надо проставить на саму модалку: */
-                if($(instance.ev[0]).hasClass('mfp-handler-left-side')) {
-                    instance.wrap.addClass('mfp-left-side');
+                if ($textareas.length) {
+                    $textareas.each(function () {
+                        expandTextarea($(this));
+                    });
                 }
+
+                // Автозапуск видео
+                const video = $(instance).find('video');
+
+                if (video.length) {
+                    $(instance).find('video')[0].play();
+                }
+
+                // Если проставлена опция mfp-handler-left-side (класс на хендлере), то её надо проставить на саму модалку
+                // Пока что это единственная опция, которая нужно на уровне mfp. Остальные ищи в виде CSS-классов
+                // в html коде прям на "window", типа window--floating-close-button или window--white-close-button
+                if ($(handler).hasClass('mfp-handler-left-side')) {
+                    $(instance).addClass('mfp-left-side');
+                }
+
+                // Шапка фиксированная, ей тоже надо корректировать пропавшее пространство под скроллбаром
+                // как это делается на теге html при открытии
+                $fixedHeader.css({'margin-right': scrollWidth});
             },
             close: function () {
                 $fixedHeader.css({'margin-right': '0'});
@@ -401,26 +449,8 @@
         type: 'image',
         removalDelay: 200,
         showCloseBtn: false,
-        callbacks: {
-            open: function () {
-
-                // Перезапускаем обсчёт expanding textareas для инстансов внутри откртой модалки
-                const instance = $.magnificPopup.instance;
-                const modalContent = instance.content[0];
-                const textareas = $(modalContent).find('.input--expandable .input__widget');
-
-                textareas.each(function () {
-                    expandTextarea($(this));
-                });
-
-                /* Шапка фиксированная, ей тоже надо корректировать пропавшее пространство подскроллбаром */
-                $fixedHeader.css({'margin-right': scrollWidth});
-            },
-            close: function () {
-                $fixedHeader.css({'margin-right': '0'});
-            }
-        }
     });
+
 
 
     /* Маска для телефона -- используем старую версию input.mask
@@ -455,7 +485,7 @@
 
     let rememberedPageScrollPosition = 0;
 
-    $('.header__toggle-menu').on('click', function () {
+    $('.header__toggle-menu, .header-275__show-menu').on('click', function () {
 
         if (!$html.hasClass('burger-expanded')) {
 
@@ -481,7 +511,7 @@
 
 
     $(document).on('click', function (event) {
-        if (!$(event.target).closest('.header__dropdown, .header__toggle-menu, .header__detachable-part').length) {
+        if (!$(event.target).closest('.header,  .header-275').length) {
             $html.removeClass('burger-expanded');
         }
     });
@@ -495,7 +525,7 @@
 
     $('.header__close-menu').on('click', function () {
         $html.removeClass('burger-expanded');
-    })
+    });
 
 
     /* Аккордеон */
@@ -1232,6 +1262,104 @@
         }
 
     });
+
+
+    /* Анимация бегущих строк */
+
+    $(function() {
+
+        // Базовая скорость в пикселях в секунду
+        const speed = 250;
+
+        // Коррекция скорости: чтобы разные строки с разной скоростью двигались
+        const speedMultiplierTop = 1.0;    // Стандартная скорость
+        const speedMultiplierMiddle = 1.1; // Быстрее
+        const speedMultiplierBottom = 0.75; // Медленнее
+
+        const lines = $('.marquee__line');
+        const states = [];
+
+        lines.each(function() {
+            const line = $(this);
+            let angle;
+            let speedMultiplier;
+
+            // Вбиванием углы (значения из фигмы) и коэффициенты скорости заданные выше
+            if (line.hasClass('marquee__line--top')) {
+                angle = 1.8;
+                speedMultiplier = speedMultiplierTop;
+            } else if (line.hasClass('marquee__line--middle')) {
+                angle = -6.52;
+                speedMultiplier = speedMultiplierMiddle;
+            } else if (line.hasClass('marquee__line--bottom')) {
+                angle = 3.98;
+                speedMultiplier = speedMultiplierBottom;
+            }
+
+            // Начальные значения (аналог нуля)
+            line.css('transform', `rotate(${angle}deg) translateX(0)`);
+
+            // Ширина каждой marquee__sentence (все одинаковые, считаем по первой)
+            const sentence = line.find('.marquee__sentence').first();
+            const sentenceWidth = sentence.outerWidth(true);
+
+            // Направление: -1 справа налево, 1 слева направо
+            const direction = line.hasClass('marquee__line--middle') ? 1 : -1;
+
+            // Подсчёт для каждой из линиий
+            states.push({
+                line,
+                left: 0,
+                sentenceWidth,
+                direction,
+                angle,
+                speedMultiplier
+            });
+        });
+
+        let previousTime = null;
+
+        function animate(time) {
+            if (previousTime !== null) {
+                const deltaTime = time - previousTime;
+                const movement = speed * deltaTime / 1000; // за кадр
+
+                states.forEach(function(state) {
+
+                    // Корректируем на множитель
+                    state.left += state.direction * movement * state.speedMultiplier;
+
+                    // справа налево
+                    if (state.direction === -1) {
+                        while (state.left <= -state.sentenceWidth) {
+                            const firstSentence = state.line.find('.marquee__sentence').first();
+                            const clone = firstSentence.clone();
+                            state.line.append(clone);
+                            firstSentence.remove();
+                            state.left += state.sentenceWidth;
+                        }
+                    }
+                    // слева направо
+                    else {
+                        while (state.left >= state.sentenceWidth) {
+                            const lastSentence = state.line.find('.marquee__sentence').last();
+                            const clone = lastSentence.clone();
+                            state.line.prepend(clone);
+                            lastSentence.remove();
+                            state.left -= state.sentenceWidth;
+                        }
+                    }
+
+                    state.line.css('transform', `rotate(${state.angle}deg) translateX(${state.left}px)`);
+                });
+            }
+            previousTime = time;
+            requestAnimationFrame(animate);
+        }
+
+        requestAnimationFrame(animate);
+    });
+
 
 })(jQuery);
 
